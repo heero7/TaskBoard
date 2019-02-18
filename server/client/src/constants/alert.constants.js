1fe5ed89a81a6eeee07eef867e10bdb53422adc6
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-02-18 16:31:11 -0600

    Moved client to another git repository. - This will allow the server to be containerized by Docker - Can develop client code with server running as a container

--- server/client/src/constants/alert.constants.js
+++
@@ -1,5 +0,0 @@
-export const alertConstants = {
-    SUCCESS : "ALERT_SUCCESS",
-    FAILURE : "ALERT_FAILURE",
-    CLEAR : "ALERT_CLEAR"
-};
--- server/client/src/reducers/users.reducer.js
+++
@@ -1,7 +0,0 @@
-import { userConstants } from "../constants";
-
-export function users(state = {}, action) {
-    switch(action) {
-        //todo : need this??
-    }
-}
