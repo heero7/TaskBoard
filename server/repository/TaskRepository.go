f3856840c22eff66808e7dc987a7d20cb8be30a5
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2019-02-18 23:27:50 -0600

    Working on TaskRepository and TaskService

--- server/repository/TaskRepository.go
+++ server/repository/TaskRepository.go
@@ -1,6 +1,11 @@
 package repository
 
 import (
+	"TaskBoard/server/models"
+	"TaskBoard/server/util"
+	"log"
+	"net/http"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,5 +16,77 @@ type TaskRepository struct {
 
 // NewTaskRepository : Creates a new instance of the TaskRepository
 func NewTaskRepository(database *gorm.DB) *TaskRepository {
-	return nil
+	return &TaskRepository{database}
+}
+
+// CreateTask : Creates a new task for the user
+func (taskRepo *TaskRepository) CreateTask(name string, priority int, uid string) map[string]interface{} {
+	tuid, err := GenerateUID()
+
+	if err != nil {
+		log.Println("Could not create a UID for this task")
+		return util.Message(http.StatusInternalServerError, "Error creating task")
+	}
+
+	task := &models.Task{
+		Name:     name,
+		Priority: priority,
+		UID:      uid,
+		TID:      tuid,
+	}
+
+	taskRepo.database.Create(task)
+
+	gr := util.Message(http.StatusOK, "Successfully added a task")
+	return gr
+}
+
+// GetTaskByID : Get a task by ID
+func (taskRepo *TaskRepository) GetTaskByID(taskid string) map[string]interface{} {
+	task := &models.Task{}
+
+	err := taskRepo.database.Table("tasks").Where("t_id = ?", taskid).First(task).Error
+
+	if err != nil {
+		log.Println("Error looking up task by Id, ", err.Error())
+		return util.Message(http.StatusInternalServerError, "Could not find task")
+	}
+
+	gr := util.Message(http.StatusOK, "Success getting task")
+	gr["task"] = task
+	return gr
+}
+
+// GetAllTasks : Get all tasks
+func (taskRepo *TaskRepository) GetAllTasks(userid string) map[string]interface{} {
+	tasks := make([]*models.Task, 0)
+
+	err := taskRepo.database.Table("tasks").Where("uid = ?", userid).Find(&tasks).Error
+	if err != nil {
+		log.Println("Error getting all tasks for this user", err.Error())
+		return util.Message(http.StatusInternalServerError, "Could not find tasks")
+	}
+
+	gr := util.Message(http.StatusOK, "Success found tasks")
+	gr["tasks"] = tasks
+	return gr
+}
+
+// UpdateTaskByID : Update the task
+func (taskRepo *TaskRepository) UpdateTaskByID(taskid string) map[string]interface{} {
+	//todo: Will need a clever way of implementing this
+	taskRes := taskRepo.GetTaskByID(taskid)
+	if taskRes["status"] == 500 {
+		log.Println("Could not get task from GetTaskByID")
+		return taskRes
+	}
+	task := taskRes["task"]
+	err := taskRepo.database.Model(&task).Update("name", "priority").Error
+
+	if err != nil {
+		log.Println("Could not update task")
+		return util.Message(http.StatusInternalServerError, "Could not update task")
+	}
+
+	return util.Message(http.StatusAccepted, "Success updating task")
 }
--- server/service/TaskService.go
+++ server/service/TaskService.go
@@ -15,3 +15,10 @@ type TaskService struct {
 func NewTaskService(config *models.Config, taskRepo *repository.TaskRepository) *TaskService {
 	return &TaskService{config: config, taskRepo: taskRepo}
 }
+
+// CreateTask : Creates a new task via the repository layer
+func (service *TaskService) CreateTask(name string, priority int, uid string) map[string]interface{} {
+	return service.taskRepo.CreateTask(name, priority, uid)
+}
+
+
